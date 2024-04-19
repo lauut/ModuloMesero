3a9edaf765c78d3ec605ef0484046cb65f951361
esme <esmeralda.cardona1@catolica.edu.sv>
2024-04-18 22:29:08 -0600

    Avance en vista de mesas

--- ModuloMesero/Controllers/mesasController.cs
+++ ModuloMesero/Controllers/mesasController.cs
@@ -1,11 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ModuloMesero.Models;
 
 namespace ModuloMesero.Controllers
 {
     public class mesasController : Controller
     {
+        private readonly DulceSaborContext _dulceSaborDbContext;
+
+        public mesasController(DulceSaborContext DulceSaborDbContext)
+        {
+            _dulceSaborDbContext = DulceSaborDbContext;
+        }
         public IActionResult Index()
         {
+        //    var ListaDeMesas = (from m in _dulceSaborDbContext.mesas select m).ToList();
+
+        //    ViewData["ListadoDeMesas"] = new SelectList(ListaDeMesas, "id_mesa", "cantidad_personas", "id_estado", "nombre_mesa");
+
             return View();
         }
     }
--- ModuloMesero/Models/DulceSaborContext.cs
+++ ModuloMesero/Models/DulceSaborContext.cs
@@ -3,6 +3,15 @@ namespace ModuloMesero.Models
 {
     public class DulceSaborContext : DbContext
     {
-        public DulceSaborContext(DbContextOptions options) : base(options) { }
+        public DulceSaborContext(DbContextOptions options) : base(options) 
+        { 
+            
+        }
+
+        public DbSet<mesas> mesas { get; set; }
+        public DbSet<estados> estados { get; set; }
+        public DbSet<Comentarios> comentarios { get; set; }
+        public DbSet<Cuenta> cuentas { get; set; }
+        public DbSet<Detalle_Pedido> detalle_Pedidos { get; set; }
     }
 }
