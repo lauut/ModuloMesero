89fee7f5c982950b19b2a7dfbba2fa364e4f33f2
lauut <lauutjd23@gmail.com>
2024-04-21 21:55:03 -0600

    vista de promociones y combos:)

---
+++ ModuloMesero/Models/categorias.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModuloMesero.Models
+{
+    public class categorias
+    {
+        [Key]
+        public int id_categoria { get; set; }
+        public string categoria { get; set; }
+
+     
+
+    }
+}
---
+++ ModuloMesero/Models/promociones.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModuloMesero.Models
+{
+    public class promociones
+    {
+        [Key]
+        public int id_promo { get; set; }
+        public string descripcion { get; set; }
+        public decimal precio {  get; set; }
+        public DateTime fecha_inicio {  get; set; }
+        public DateTime fecha_fin {  get; set; }
+        public int id_estado {  get; set; }
+        public string nombre { get; set; }
+        public string imagen {  get; set; }
+
+
+
+    }
+}
