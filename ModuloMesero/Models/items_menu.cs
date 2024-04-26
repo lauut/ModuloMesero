8929369be60ba5af8b29f4281f07d42ef3b0239f
esme <esmeralda.cardona1@catolica.edu.sv>
2024-04-26 13:18:31 -0600

    Ajustes en layout, detalles pedidp

--- ModuloMesero/Models/items_menu.cs
+++ ModuloMesero/Models/items_menu.cs
@@ -6,12 +6,12 @@ namespace ModuloMesero.Models
     {
         [Key]
         public int id_item_menu {  get; set; }
-        public string nombre { get; set; }
-        public string descripcion {  get; set; }
-        public double precio { get; set; }
-        public string imagen { get; set; }
-        public int id_estado { get; set; }
-        public int id_categoria { get; set; }
+        public string? nombre { get; set; }
+        public string? descripcion { get; set; }
+        public decimal? precio { get; set; }
+        public string? imagen { get; set; }
+        public int? id_estado { get; set; }
+        public int? id_categoria { get; set; }
 
 
     }
