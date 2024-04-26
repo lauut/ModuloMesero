428a5061d9bda2e188efe391b9f1a764b54bf646
esme <esmeralda.cardona1@catolica.edu.sv>
2024-04-26 13:06:49 -0600

    :)

---
+++ ModuloMesero/Controllers/MenuOptionsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModuloMesero.Controllers
+{
+    public class MenuOptionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
