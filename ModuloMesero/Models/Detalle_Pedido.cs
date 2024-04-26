f5835fb5939ef5f3b2aa5d7a922919bad87d4701
esme <esmeralda.cardona1@catolica.edu.sv>
2024-04-25 22:48:00 -0600

    Listar pedido

--- ModuloMesero/Models/Detalle_Pedido.cs
+++ ModuloMesero/Models/Detalle_Pedido.cs
@@ -8,6 +8,7 @@ namespace ModuloMesero.Models
         public int Id_DetalleCuenta { get; set; }
         public int Id_cuenta { get; set; }
         public int Id_plato { get; set; }
+        public int Cantidad { get; set; }
         public string Estado { get; set; }
         public string Tipo_Plato { get; set; }
         public double Precio { get; set; }
