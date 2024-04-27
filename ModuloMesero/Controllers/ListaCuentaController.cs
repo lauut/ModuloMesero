5a35f318a71409cb356991237892f4c06bdb00b7
esme <esmeralda.cardona1@catolica.edu.sv>
2024-04-26 22:56:17 -0600

    Retoques en cuentas

---
+++ ModuloMesero/Controllers/ListaCuentaController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using ModuloMesero.Models;
+
+namespace ModuloMesero.Controllers
+{
+    public class ListaCuentaController : Controller
+    {
+        private readonly DulceSaborContext _DulceSaborContext;
+        public ListaCuentaController(DulceSaborContext DulceSaborContext)
+        {
+
+            _DulceSaborContext = DulceSaborContext;
+
+
+        }
+
+        public IActionResult Index()
+        {
+            var listacuentas = (from c in _DulceSaborContext.Cuenta
+                                join m in _DulceSaborContext.mesas on c.Id_mesa equals m.id_mesa
+                                select new
+                                {
+                                    nombre = c.Nombre,
+                                    idcuenta = c.Id_cuenta,
+                                    nombremesa = m.nombre_mesa,
+                                    m.id_mesa
+
+                                }).ToList();
+            ViewData["listcuenta"] = listacuentas;
+
+
+            return View();
+
+        }
+        
+    }
+}
